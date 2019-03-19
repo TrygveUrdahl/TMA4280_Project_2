82de10ecf078337e07e5838fc76fd3da69b92732
Trygve Scheline Urdahl <trygve.urdahl@hotmail.com>
2019-03-19 18:42:55 +0100

    CMakeList setup with libraries and includes

---
+++ src/extern.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+extern "C" {
+  void fst_(double *v, int *n, double *w, int *nn);
+  void fstinv_(double *v, int *n, double *w, int *nn);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 
+#include "extern.hpp"
+#include "omp.h"
+
 int main(int argc, char** argv) {
 
   return 0;
